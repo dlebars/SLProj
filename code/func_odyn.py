9a8e87d738c5fa5ab27fa3bbf428dc841cb6341c
dlebars <dewi.lebars@gmail.com>
2019-12-21 22:51:56 +0100

    Fix a bug in odyn_glob_ipcc related to xarray dimensions

--- code/func_odyn.py
+++ code/func_odyn.py
@@ -3,6 +3,7 @@
 #               contribution to sea level
 ################################################################################
 
+import numpy as np
 import xarray as xr
 from scipy.stats import norm
 
@@ -12,8 +13,7 @@ from scipy.stats import norm
 # odyn_glob_knmi
 
 
-def odyn_glob_ipcc(SCE:string ,DIR_IPCC:string, N[*]:numeric, \
-                        nb_y2[*]:numeric, Gam[*]:numeric, NormD[*]:numeric):
+def odyn_glob_ipcc(SCE, DIR_IPCC, N, nb_y2, Gam, NormD):
     '''Compute thermal expansion contribution to global sea level from IPCC data.'''
 
     X_O_med   = np.zeros(nb_y2-1)     # These start in 2007 instead of 2006
@@ -21,21 +21,23 @@ def odyn_glob_ipcc(SCE:string ,DIR_IPCC:string, N[*]:numeric, \
 
     f_med     = xr.open_dataset(DIR_IPCC+SCE+'_expansionmid.nc')
     f_up      = xr.open_dataset(DIR_IPCC+SCE+'_expansionupper.nc')
-    X_O_med   = f_med.global_average_sea_level_change * 100
-    X_O_up    = f_up.global_average_sea_level_change * 100
+    # Read the variables in the files. Use .values to convert the xarray 
+    # DataArrays to numpy arrays that can be multiplied more easily.
+    X_O_med   = f_med.global_average_sea_level_change.values * 100
+    X_O_up    = f_up.global_average_sea_level_change.values * 100
 
     X_O    = np.zeros([N,nb_y2])
     X_O_sd = (X_O_up - X_O_med) / norm.ppf(0.95)  # ~1.64 
 
     for t in range(1,nb_y2):
-        X_O[:,t]  = X_O_med[t-1] + Gam*NormD(:) * X_O_sd[t-1]
+        X_O[:,t]  = X_O_med[t-1] + Gam*NormD * X_O_sd[t-1]
 
     X_O[:,0]      = X_O[:,1]
 
     X_O_out        = np.zeros([3, N, nb_y2])
-    X_O_out(0,:,:) = X_O
-    X_O_out(1,:,:) = X_O   # In this case global is the same as total
-    X_O_out(2,:,:) = 0     # and anomaly is 0
+    X_O_out[0,:,:] = X_O
+    X_O_out[1,:,:] = X_O   # In this case global is the same as total
+    X_O_out[2,:,:] = 0     # and anomaly is 0
     return X_O_out
 
 
