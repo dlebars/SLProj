25cf047efa902da2cdea923667b424b529ca0e60
dlebars <dewi.lebars@gmail.com>
2020-03-03 21:21:17 +0100

    First buggy version of B19

--- code/func_B19.py
+++ code/func_B19.py
@@ -27,7 +27,7 @@ def ReadB19(path):
 
 def TempF(T0, T1, T2, t0, t1, t2):
     '''Define the high and low temperature scenarios from Bamber et al. 2019'''
-    tf = np.arange(t0, t2)
+    tf = np.arange(t0, t2+1)
     T = np.zeros(len(tf))
     T[0:t1-t0+1] = (T1 - T0)/(t1 - t0) * tf[0:t1-t0+1] + (t1*T0 - t0*T1)/(t1-t0)
     T[t1-t0+1:t2-t0+1] = (T2 - T1)/(t2 - t1) * tf[t1-t0+1:t2-t0+1] + (t2*T1 - t1*T2)/(t2-t1)
@@ -36,15 +36,15 @@ def TempF(T0, T1, T2, t0, t1, t2):
 
 def TempInterp(IceSheet, Tdcum, Tlcum, Thcum, PAR, UnifP):
     '''Temperature interpolation function'''
-    dimTd = Tdcum.shape()
-    N = dimTd[0]
+    N = Tdcum.shape[0]
+    ind0 = 0  
     if IceSheet == 'GIS':
         i = 0
-    else if IceSheet == 'WAIS':
+    elif IceSheet == 'WAIS':
         i = 1
-    else if IceSheet == 'EAIS':
+    elif IceSheet == 'EAIS':
         i = 2
-    else
+    else:
         print('ERROR: Ice sheet name '+IceSheet+' not recognised')
 
     PARtT = np.zeros([3, 2, N]) # PARAM, TIME, DRAWS
@@ -59,57 +59,108 @@ def TempInterp(IceSheet, Tdcum, Tlcum, Thcum, PAR, UnifP):
     # Could be much faster with categories of similar shape
     xg = np.zeros([2, N]) # DRAWS, TIME
     for r in range(0,N):
-    for t in range (0,2):
-        if PARtT[0, t, r] <= 0:
-            print('Warning: PARtT[0, t, r] <= 0, setting to 0.01')
-            PARtT[0, t, r] = 0.01
-        xg[t, r] = stats.gamma.ppf( UnifP[r], PARtT[0, t, r], 0, 1)
+        for t in range (0,2):
+            if PARtT[0, t, r] <= 0:
+                # Cummulate the number of times this happens
+                PARtT[0, t, r] = 0.01
+                ind0 = ind0+1
+            if ind0 > 0:
+                print('Warning: For period'+ str(t) +' the shape parameter was' +\
+                      'set to 0.01 '+ str(ind0) + 'times out of' + str(N) + \
+                      'samples to avoid negative values')
+            xg[t, r] = stats.gamma.ppf( UnifP[r], PARtT[0, t, r], 0, 1)
 
     # Apply scale and location parameters
-    ICcont = xg * PARtT[2, :, :] + PARtT[1, :, :]
+    IS_cont = xg * PARtT[2, :, :] + PARtT[1, :, :]
 
-    return ICcont
+    return IS_cont
 
 
-# def TimeInterp(IS_cont, td, v0):
-#     '''Time interpolation between present day mass loss and future SEJ data'''
+def TimeInterp(IS_cont, td, v0):
+    '''Time interpolation between present day mass loss and future SEJ data'''
 
-#     dimIS = dimsizes(IS_cont)
-#     N = dimIS(1)
-#     tf = ispan(td(0),td(2),1)
-#     dimt = dimsizes(tf)
+    N = IS_cont.shape[1]
+    tf = np.arange(td[0],td[2]+1)
 
-#     t0 = td(0)
-#     t1 = td(1)
-#     t2 = td(2)
+    t0 = td[0]
+    t1 = td[1]
+    t2 = td[2]
 
-#     ;# Extrapollate using a third order polynomial:
-#     ; X(t) = p3*(t-t0)^3 + p2*(t-t0)^2 + p1*(t-t0) + p0
-#     ; Using three conditions:
-#     ; X(t0) = 0
-#     ; X'(t0) = v0
-#     ; X(t1) = X1
-#     ; X(t2) = X2
+    ## Extrapollate using a third order polynomial:
+    # X(t) = p3*(t-t0)^3 + p2*(t-t0)^2 + p1*(t-t0) + p0
+    # Using three conditions:
+    # X(t0) = 0
+    # X'(t0) = v0
+    # X(t1) = X1
+    # X(t2) = X2
 
-#     X1 = IS_cont(0,:)
-#     X2 = IS_cont(1,:)
+    X1 = IS_cont[0,:]
+    X2 = IS_cont[1,:]
 
-#     p0 = 0
-#     p1u = fspan(v0(0), v0(1), N)
-#     ;Change the order of p1u to fit the order of IS_cont
-#     ISpv  = dim_pqsort(IS_cont(0,:), 1)
-#     ISpv2 = dim_pqsort(ISpv, 1)
-#     p1 = p1u(ISpv2)
+    p0 = 0
+    p1u = np.linspace(v0[0], v0[1], num=N)
+    # Change the order of p1u to fit the order of IS_cont    
+    ISpv = np.argsort(IS_cont[0,:])
+    ISpv2 = np.argsort(ISpv)
+    p1 = p1u[ISpv2]
+    # Check
+    print('Compute stats.spearmanr(p1, IS_cont[0,:], result should be 1:')
+    print(stats.spearmanr(p1, IS_cont[0,:]))
+    print('Compute stats.spearmanr(p1u, IS_cont[0,:], result should be 0:')
+    print(stats.spearmanr(p1u, IS_cont[0,:]))
+    
+    Pa = X1 - p1*(t1 - t0) - p0
+    Pb = X2 - p1*(t2 - t0) - p0
 
-#     Pa = X1 - p1*(t1 - t0) - p0
-#     Pb = X2 - p1*(t2 - t0) - p0
+    p3 = (Pa*(t2-t0)**2 - Pb*(t1-t0)**2) / \
+    ( (t1-t0)**3 * (t2-t0)**2 - (t2-t0)**3 * (t1-t0)**2 )
+    p2 = (Pa - p3 * (t1-t0)**3) / (t1-t0)**2
 
-#     p3 = (Pa*(t2-t0)^2 - Pb*(t1-t0)^2) / ( (t1-t0)^3*(t2-t0)^2 - (t2-t0)^3*(t1-t0)^2 )
-#     p2 = (Pa - p3*(t1-t0)^3) / (t1-t0)^2
+    IS_cont_t = np.zeros([N, len(tf)])
+    for t in range(0, len(tf)):
+        IS_cont_t[:, t] = p3 * t**3 + p2 * t**2 + p1*t + p0
 
-#     IS_cont_t = new((/N, dimt/), float)
-#     do t=0,dimt-1
-#     IS_cont_t(:, t) = p3*t^3 + p2*t^2 + p1*t + p0
-#     end do
+    return IS_cont_t
 
-#     return IS_cont_t
+def Bamber19(IceSheet, UnifP, v0, t0, Td):
+    '''Combine all previous functions to provide the final ice sheet 
+    contribution Works for IceSheet is 'GIS', 'WAIS' or 'EAIS' '''
+    # Read the parameters data: Shape, location and scale
+    # Option: parameters.csv and parameters_mean_std_opt.csv that differ in the
+    # way that the optimisation is performed to fit the distribution
+    path = "../../BamberDataDistribution/parameters_mean_std_opt.csv" 
+    PAR = ReadB19(path)
+
+    t1 = 2050
+    t2 = 2100
+    tf = np.arange(t0, t2+1)
+
+    ## First order interpolation along temperature integral
+    # Define the temperature functions for the low and high scenarios
+    T0 = 0.8     # Should be floats, integers lead to problem in the computations
+    Tl50 = 1.5
+    Tl100 = 2.
+    Th50 = 2.
+    Th100 = 5.
+
+    Tl = TempF(T0, Tl50, Tl100, 2000, t1, t2)
+    Th = TempF(T0, Th50, Th100, 2000, t1, t2)
+    Tlcum = Tl.cumsum()
+    Thcum = Th.cumsum()
+
+    Tdcum = Td.cumsum(axis=1)
+    indt = np.zeros(2, dtype=int)
+    indt[0] = abs(tf - t1).argmin()
+    indt[1] = abs(tf - t2).argmin()
+
+    ## Temperature interpollation
+    IS_cont = TempInterp(IceSheet, Tdcum[:,indt], Tlcum[indt], Thcum[indt], PAR, UnifP)
+
+    ## Time interpolation:
+    # Start from present day observed rates of melt (or AR5 range)
+    # Interpolate through the future using a 3rd order polynomial. 
+    # To add data for latter dates a higher order polynomial might be used
+
+    IS_cont_t = TimeInterp(IS_cont, [t0, t1, t2], v0)
+
+    return IS_cont_t
