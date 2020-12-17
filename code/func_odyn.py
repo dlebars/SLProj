33540b0b40c5a0ddbd6c1cf5f837c7819a812158
dlebars <dewi.lebars@gmail.com>
2020-12-17 10:00:41 +0100

    Further use of Xarray in odyn_loc

--- code/func_odyn.py
+++ code/func_odyn.py
@@ -6,67 +6,70 @@ import numpy as np
 import xarray as xr
 from scipy.stats import norm
 
+#Change inputs -> make it easy to change the reference period
 def odyn_loc(SCE, MOD, nb_y2, DIR_O, DIR_OG, lat_N, lat_S, lon_W, lon_E, \
-             start_date, ye, SSH_VAR, N, i_ys, Gam, NormD):
+             start_date, ye, SSH_VAR, N, ys, Gam, NormD, LowPass):
     '''Compute the ocean dynamics and thermal expansion contribution to local sea
     level.'''
 
     nb_MOD = len(MOD)
     nb_y = ye-start_date+1
 
-    # Initialize the SSH matrix: (scenario, model, time (years))
-    MAT   = np.zeros([nb_MOD,nb_y])
-    MAT_G = np.zeros([nb_MOD,nb_y]) # Global mean steric effect
-    MAT_A = np.zeros([nb_MOD,nb_y]) # Local dynamics
-
+    # Read sterodynamics and global steric contributions
     for m in range(nb_MOD):
         fi = xr.open_dataset(f'{DIR_O}{MOD[m]}_{SCE}.nc', use_cftime=True)
         fig = xr.open_dataset(f'{DIR_OG}{MOD[m]}_{SCE}.nc', use_cftime=True)
         fi = fi.assign_coords(TIME=fi['TIME.year'])
         fig = fig.assign_coords(time=fig['time.year']).squeeze()
-        SSH = fi[SSH_VAR].sel(TIME=slice(start_date,ye), 
-                              latitude=slice(lat_S,lat_N), 
-                              longitude=slice(lon_W,lon_E))
-        print(SSH.shape)
-
-        if SSH.TIME[-1].values.item() == ye:
-            MAT[m,:] = SSH.mean(dim=['latitude', 'longitude'])
-            #RQ: No scaling depending on the area, gives more weight to the southern
-            #cell
-            MAT_G[m,:] = fig[SSH_VAR].sel(time=slice(start_date,ye))
-        else: # For models that stop in 2099
-            MAT[m, :nb_y-1] = SSH.mean(dim=['latitude', 'longitude'])
-            MAT[m,nb_y-1] = MAT[m,nb_y-2]
-            MAT_G[m,:nb_y-1] = fig[SSH_VAR].sel(time=slice(start_date,ye))
-            MAT_G[m,nb_y-1] = MAT_G[m,nb_y-2]
-
-        # Remove the average SSH of the first 20 years from all models
-        MAT[m,:] =  MAT[m,:] - MAT[m,:21].mean()
-
-        MAT_G[m,:] = MAT_G[m,:] - MAT_G[m,:21].mean()
-        MAT_A[m,:] = MAT[m,:] - MAT_G[m,:]
+        sd_da = fi[SSH_VAR].assign_coords(model=MOD[m])
+        st_da = fig[SSH_VAR].assign_coords(model=MOD[m])
         
-        del(fi)
-        del(SSH)
+        if m==0:
+            full_sd_da = sd_da
+            full_st_da = st_da
+        else:
+            full_sd_da = xr.concat([full_sd_da, sd_da], dim='model')
+            full_st_da = xr.concat([full_st_da, st_da], dim='model')
+    
+    full_sd_da = full_sd_da.rename({'TIME':'time'})
+    full_sd_da = full_sd_da.sel(time=slice(start_date,ye), 
+                            latitude=slice(lat_S,lat_N), 
+                            longitude=slice(lon_W,lon_E))
+    
+    MAT = full_sd_da.mean(dim=['latitude', 'longitude'])
+    MAT_G = full_st_da.sel(time=slice(start_date,ye))
 
-    MATs     = MAT[:,i_ys:]*100   # Convert from m to cm
-    MAT_Gs   = MAT_G[:,i_ys:]*100 # Convert from m to cm
-    MAT_As   = MAT_A[:,i_ys:]*100 # Convert from m to cm
-    #Build the distribution
-    X_O_m    = MATs.mean(axis=0) # Compute the inter-model mean for each time
-    X_O_sd   = MATs.std(axis=0)  # Compute the inter-model standard deviation
-    X_O_G_m  = MAT_Gs.mean(axis=0)
-    X_O_G_sd = MAT_Gs.std(axis=0)
-    X_O_A_m  = MAT_As.mean(axis=0)
-    X_O_A_sd = MAT_As.std(axis=0)
+    MAT = MAT - MAT.sel(time=slice(start_date,start_date+20)).mean(dim='time')
+    MAT_G = MAT_G - MAT_G.sel(time=slice(start_date,start_date+20)).mean(dim='time')
+                        
+    MAT_A = MAT - MAT_G
+                
+    if LowPass:
+        #TODO
+        np.polyfit(x, MAT[m,:], 3)
 
-    X_O    = np.zeros([N,nb_y2])
-    X_O_G  = np.zeros([N,nb_y2])
-    X_O_A  = np.zeros([N,nb_y2])
+    # Select years after the reference period and convert from m to cm
+    MAT = MAT.sel(time=slice(ys,None))*100
+    MAT_Gs = MAT_G.sel(time=slice(ys,None))*100
+    MAT_As = MAT_A.sel(time=slice(ys,None))*100
+    
+    # Build the distributions
+    # If a model has missing data it is not included in the mean and standard deviation.
+    # Another possibility would be to fill the missing data first.
+    X_O_m = np.array(MAT.mean(dim='model')) # Compute the inter-model mean for each time
+    X_O_sd = np.array(MAT.std(dim='model')) # Compute the inter-model standard deviation
+    X_O_G_m = np.array(MAT_Gs.mean(dim='model'))
+    X_O_G_sd = np.array(MAT_Gs.std(dim='model'))
+    X_O_A_m = np.array(MAT_As.mean(dim='model'))
+    X_O_A_sd = np.array(MAT_As.std(dim='model'))
+
+    X_O = np.zeros([N,nb_y2])
+    X_O_G = np.zeros([N,nb_y2])
+    X_O_A = np.zeros([N,nb_y2])
     for t in range(nb_y2):
-        X_O[:,t]    = X_O_m[t] + Gam * NormD * X_O_sd[t]
-        X_O_G[:,t]  = X_O_G_m[t] + Gam * NormD * X_O_G_sd[t]
-        X_O_A[:,t]  = X_O_A_m[t] + Gam * NormD * X_O_A_sd[t]
+        X_O[:,t]   = X_O_m[t] + Gam * NormD * X_O_sd[t]
+        X_O_G[:,t] = X_O_G_m[t] + Gam * NormD * X_O_G_sd[t]
+        X_O_A[:,t] = X_O_A_m[t] + Gam * NormD * X_O_A_sd[t]
 
     X_O_out = np.zeros([3,N,nb_y2])
     X_O_out[0,:,:] = X_O
@@ -82,7 +85,7 @@ def odyn_loc(SCE, MOD, nb_y2, DIR_O, DIR_OG, lat_N, lat_S, lon_W, lon_E, \
 def odyn_glob_ipcc(SCE, DIR_IPCC, N, nb_y2, Gam, NormD):
     '''Compute thermal expansion contribution to global sea level from IPCC data.'''
 
-    X_O_med   = np.zeros(nb_y2-1) # These start in 2007 instead of 2006
+    X_O_med   = np.zeros(nb_y2-1) # In the files 2007 means the average over 2006
     X_O_up    = np.zeros(nb_y2-1)
 
     f_med     = xr.open_dataset(DIR_IPCC+SCE+'_expansionmid.nc')
