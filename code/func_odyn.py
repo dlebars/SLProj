c5d1c18063e42de49401e8540b89c73f998db6b1
dlebars <dewi.lebars@gmail.com>
2020-12-29 15:43:05 +0100

    Add the possibility to odyn_cmip to do global projections and fix a bug that appeared when LowPass was used with ye=2100

--- code/func_odyn.py
+++ code/func_odyn.py
@@ -96,53 +96,51 @@ def odyn_glob_ipcc(SCE, DIR_IPCC, N, nb_y2, Gam, NormD):
     X_O_out[2,:,:] = 0     # and anomaly is 0
     return X_O_out
 
-
-# read_odyn_cmip5
-
 def odyn_cmip(SCE, DIR_CMIP, LOC, ref_steric, ye, N, ys, Gam, NormD, LowPass):
     '''Read the CMIP5 and CMIP6 global steric and ocean dynamics contribution
-    and compute a probability distribution'''
+    and compute a probability distribution.'''
     
     mip = misc.which_mip(SCE)
     nb_y2 = ye - ys +1
-    lat_N, lat_S, lon_W, lon_E = LOC
-
-    if mip == 'cmip5':
-        # Change the name of files to avoid this if
-        zos_ds = xr.open_mfdataset(f'{DIR_CMIP}/{mip}_zos_{SCE}/CMIP5_zos_{SCE}_*.nc')
-    else:
-        zos_ds = xr.open_mfdataset(f'{DIR_CMIP}/{mip}_zos_{SCE}/{mip}_zos_{SCE}_*.nc')
-        
-    zos_ds = misc.rotate_longitude(zos_ds, 'lon')
     
+    # Read global steric
     full_st_da = xr.open_dataset(f'{DIR_CMIP}/{mip}_SeaLevel_{SCE}_zostoga_1986_2100.nc')
-    
-    full_sd_da = zos_ds['CorrectedReggrided_zos'].sel(time=slice(ref_steric[0],ye), 
-                            lat=slice(lat_S,lat_N), 
-                            lon=slice(lon_W,lon_E))
-    
     # Convert from m to cm
     MAT_G = full_st_da['zostoga_corrected'].sel(time=slice(ref_steric[0],ye))*100
     
-    # There are more models available for zos than for zostoga
-    # Here we select the intersection
-    model_list = list(set(full_sd_da.model.values) & set(MAT_G.model.values))
-    MAT_A = full_sd_da.sel(model=model_list).mean(dim=['lat', 'lon'])
-    
-    MAT = MAT_A + MAT_G
+
+    if LOC:
+        lat_N, lat_S, lon_W, lon_E = LOC
+
+        if mip == 'cmip5':
+            # Change the name of files to avoid this if
+            zos_ds = xr.open_mfdataset(f'{DIR_CMIP}/{mip}_zos_{SCE}/CMIP5_zos_{SCE}_*.nc')
+        else:
+            zos_ds = xr.open_mfdataset(f'{DIR_CMIP}/{mip}_zos_{SCE}/{mip}_zos_{SCE}_*.nc')
+
+        zos_ds = misc.rotate_longitude(zos_ds, 'lon')
+        full_sd_da = zos_ds['CorrectedReggrided_zos'].sel(time=slice(ref_steric[0],ye), 
+                                lat=slice(lat_S,lat_N), 
+                                lon=slice(lon_W,lon_E))
+
+        # There are more models available for zos than for zostoga
+        # Here we select the intersection
+        model_list = list(set(full_sd_da.model.values) & set(MAT_G.model.values))
+        MAT_A = full_sd_da.sel(model=model_list).mean(dim=['lat', 'lon'])
+        MAT = MAT_A + MAT_G
+        
+    else:
+        MAT = MAT_G
+        MAT_A = xr.zeros_like(MAT_G)
     
     if LowPass:
-        fit_coeff = MAT.polyfit('time', 2)
         new_time = xr.DataArray( np.arange(ys,ye+1)+0.5, dims='time', 
                         coords=[np.arange(ys,ye+1)+0.5], name='time' )
-        MAT = xr.polyval(coord=new_time, 
-                         coeffs=fit_coeff.polyfit_coefficients)
-        
-        if ye>(full_sd_da.time[-1]+1):
-            fit_coeff = MAT_G.polyfit('time', 2)
-            MAT_G = xr.polyval(coord=new_time, 
-                               coeffs=fit_coeff.polyfit_coefficients)
-            MAT_A = MAT - MAT_G
+        fit_coeff = MAT.polyfit('time', 2)
+        MAT = xr.polyval(coord=new_time, coeffs=fit_coeff.polyfit_coefficients)       
+        fit_coeff = MAT_G.polyfit('time', 2)
+        MAT_G = xr.polyval(coord=new_time, coeffs=fit_coeff.polyfit_coefficients)
+        MAT_A = MAT - MAT_G
     
     list_da = [MAT, MAT_G, MAT_A]
     
@@ -151,11 +149,11 @@ def odyn_cmip(SCE, DIR_CMIP, LOC, ref_steric, ye, N, ys, Gam, NormD, LowPass):
     for idx, da in enumerate(list_da):
         # Select years after the reference period
         da = da.sel(time=slice(ys,None))
-        
-        # Add a year at the end because data is available until 2099 while code 
-        # goes up to 2100
-        dal = da.isel(time=-1).assign_coords(time=da.time[-1]+1) # Last year with new coords
-        da = xr.concat([da, dal], dim='time')
+        if not(LowPass):
+            # Add a year at the end because data is available until 2099 while code 
+            # goes up to 2100
+            dal = da.isel(time=-1).assign_coords(time=da.time[-1]+1) # Last year with new coords
+            da = xr.concat([da, dal], dim='time')
         
         X_O_out[idx,:,:] = misc.normal_distrib(da, Gam, NormD)
 
