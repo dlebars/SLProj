711d80a0f5d6464b184ea0e87d65ed5a935a03a1
Dewi Le Bars <dewi.lebars@gmail.com>
2023-04-11 17:07:40 +0200

    Update namelist_KNMI23.py

--- namelists/namelist_KNMI23.py
+++ namelists/namelist_KNMI23.py
@@ -1,13 +1,18 @@
 INFO = True
 LastYear = 2100 # Last year of the computation 2100 or 2125.
 # Original box from Hylke: 51-60N, -3.5,7.5E
-LOC = [60, 51, -3.5, 7.5] # lat_N, lat_S, lon_W, lon_E
+#LOC = [60, 51, -3.5, 7.5] # lat_N, lat_S, lon_W, lon_E
+# Define a polygon for the Dutch coast
+LOC = [[2.5, 53], [3.3, 51.5], [4.25, 52.25], [4.75, 53.3], [5.5, 53.6], 
+       [7, 53.75], [7, 55], [4, 54.5]]
 LOC_FP = [53, 5] # lat, lon
 IBarE = False
 TEMP = 'AR6'
 GRE = 'IPCC'
 ODYN = 'CMIP6'
-ODSL_LIST = ['ACCESS-ESM1-5', 'CAMS-CSM1-0', 'CESM2-WACCM', 'CNRM-CM6-1', 'CNRM-ESM2-1', 'EC-Earth3', 'FGOALS-f3-L', 'MIROC-ES2L', 'MPI-ESM1-2-LR', 'NESM3', 'NorESM2-LM', 'NorESM2-MM', 'TaiESM1', 'UKESM1-0-LL']
+ODSL_LIST = ['ACCESS-ESM1-5', 'CESM2-WACCM', 'CNRM-CM6-1', 'CNRM-ESM2-1', 
+             'EC-Earth3', 'GISS-E2-1-G', 'INM-CM4-8', 'MIROC-ES2L', 
+             'MPI-ESM1-2-LR', 'NESM3', 'NorESM2-LM', 'NorESM2-MM', 'UKESM1-0-LL']
 ANT_DYN = 'KNMI23'
 GIC = 'AR6'
 GAM = 1
@@ -23,7 +28,7 @@ NoU_G = False
 NoU_A = False
 NoU_Gl = False
 Decomp = False
-LowPass = False
+LowPass = 3
 # Assume wind trend over the last 30 years is natural variability
-BiasCorr = 0.4 #0.5
+BiasCorr = False #0.4 #0.5
 InterpBack = True
