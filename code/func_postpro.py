dd6e6c11c58f19635a15bf970c5d534e7d6d0bf1
Dewi Le Bars <dewi.lebars@gmail.com>
2023-09-06 11:31:58 +0200

    Adds add_subsidence
    Adds add_subsidence as input instead of hard coded

--- code/func_postpro.py
+++ code/func_postpro.py
@@ -4,19 +4,26 @@
 import numpy as np
 import pandas as pd
 
-def read_knmi14(add_subsidence=False):
+def read_knmi14(add_subsidence=False, polyfit=False):
     '''Read KNMI14 projections from csv file.
-    Possibility to add subsidence with a value of 0.045 cm/y as in the 
-    Zeespiegelmonitor.
+    Possibility to add subsidence with a value in cm/y.
+    'polyfit' provides a way to choose to read the original data or the data 
+    after fitting a 3rd degree polynomial
     Export a handy dataframe for further analysis.'''
     
     path_KNMI14 = '/Users/dewilebars/Projects/Project_ProbSLR/KNMI14/'
-    KNMI14_df = pd.read_csv(path_KNMI14 + 'K14_scenarios_by_year.csv')
-    del(KNMI14_df['year.1'])
+    
+    if polyfit:
+        KNMI14_df = pd.read_csv(path_KNMI14 + 'KNMI14_sealevel_scenarios_by_1year_3polfit.csv')
+        del(KNMI14_df['comment'])
+    else:
+        KNMI14_df = pd.read_csv(path_KNMI14 + 'K14_scenarios_by_year.csv')
+        del(KNMI14_df['year.1'])
+        
     KNMI14_df = KNMI14_df.set_index('year')
 
     if add_subsidence:
-        subsidence = np.arange(KNMI14_df.shape[0]) * 0.045
+        subsidence = np.arange(KNMI14_df.shape[0]) * add_subsidence
 
         for i in KNMI14_df.columns:
             KNMI14_df[i + '_sub'] = KNMI14_df[i] + subsidence
